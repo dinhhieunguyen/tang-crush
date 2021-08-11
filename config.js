2cc3fcd2c45ed6a4bed9d8e008b1f42d49aa26fc
dinhhieunguyen <88791177+dinhhieunguyen@users.noreply.github.com>
2021-08-12 02:25:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 const CONFIG = {
     introTitle: 'Slow à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'gửi em',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    introDesc: `Anh có điều muốn hỏi em
+    em hãy chọn đường thoát ra nhé
+    Trắc nghiệm vui thôi`,
+    btnIntro: 'click vào',
+    title: 'Em đồng ý làm bạn gái anh nhé 😙',
+    desc: 'Em hãy lựa chọn 1 trong 2 phương án đừng thoát nhé ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
